74002ffb4744dd5db9087eeeb49fca7be8876049
Anju <anjukumari75958@gmail.com>
2025-07-23 01:34:14 +0530

    many files added

---
+++ newfrontend/src/App.jsx
@@ -0,0 +1,53 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import './App.css'
+import Signup from './components/Signup'
+import Login from './components/Login'
+import Home from './components/home'
+import MainLayout from './components/MainLayout'
+import Profile from './Profile'
+import { Toaster } from './components/ui/sonner'
+import ProtectedRoutes from './components/ProtectedRoutes'
+const browserRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <ProtectedRoutes><MainLayout /></ProtectedRoutes>,
+    children: [
+      {
+        path: '/',
+        element: <ProtectedRoutes><Home /></ProtectedRoutes>
+      },
+      {
+        path: '/profile',
+        element: <ProtectedRoutes> <Profile /></ProtectedRoutes>
+      },
+      // {
+      //   path: '/account/edit',
+      //   element: <ProtectedRoutes><EditProfile /></ProtectedRoutes>
+      // },
+      // {
+      //   path: '/chat',
+      //   element: <ProtectedRoutes><ChatPage /></ProtectedRoutes>
+      // },
+    ]
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  },
+])
+function App() {
+
+  return (
+    <>
+      <Toaster/>
+      <RouterProvider router={browserRouter} />
+      
+    </>
+  )
+}
+
+export default App
---
+++ newfrontend/src/components/MainLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+// import { Outlet } from 'react-router-dom'
+// import LeftSidebar from './LeftSidebar'
+
+const MainLayout = () => {
+  return (
+    <div>
+         {/* <LeftSidebar/>
+        <div>
+            <Outlet/>
+        </div> */}
+        Sidebar
+    </div>
+  )
+}
+
+export default MainLayout
