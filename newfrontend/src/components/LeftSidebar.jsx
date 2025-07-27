12f82c035a09b4f70633b5e2755b891a1e8de294
Anju <anjukumari75958@gmail.com>
2025-07-27 10:00:44 +0530

    deploy

--- newfrontend/src/components/LeftSidebar.jsx
+++ newfrontend/src/components/LeftSidebar.jsx
@@ -5,13 +5,12 @@ import { toast } from 'sonner'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-
 import { setAuthUser } from '@/redux/authSlice'
 import CreatePost from './CreatePost'
 import { setPosts, setSelectedPost } from '@/redux/postSlice'
 import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
 import { Button } from './ui/button'
-
+import logo from '../assets/img/logo.png'
 const LeftSidebar = () => {
     const navigate = useNavigate();
     const { user } = useSelector(store => store.auth);
@@ -74,7 +73,7 @@ const LeftSidebar = () => {
     return (
         <div className='fixed top-0 z-10 left-0 px-4 border-r border-gray-300 w-[16%] h-screen'>
             <div className='flex flex-col'>
-                <h1 className='my-8 pl-3 font-bold text-xl'>LOGO</h1>
+                <h1 className='mt-8 pl-3 font-bold text-xl'><img src={logo} alt="YatraGo Logo" className='w-16 h-14'/></h1>
                 <div>
                     {
                         sidebarItems.map((item, index) => {
